a7395ef75c5b5be1ae4672da7f3612f18a1ee9b6
Andrey Golovushkin <frenkodetto@yandex.ru>
2025-05-14 15:34:24 +0300

    Add views base edit

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from users.models import User
+from meals.models import Tag, Ingredient, Recipe
+from api.serializers import TagSerializer, IngredientSerializer, RecipeSerializer, UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    """."""
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    pagination_class = None
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    """."""
+
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    pagination_class = None
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """."""
+
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
