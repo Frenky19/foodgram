eaf27e674c1688e7906d8a9d266083f409550ed2
Andrey Golovushkin <frenkodetto@yandex.ru>
2025-05-14 13:31:55 +0300

    urls base edit

---
+++ backend/foodgram/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('djoser.urls.authtoken')),
+]
