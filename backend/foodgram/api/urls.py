e93b974af84b6a72cd25fe8af36aedfd1c59a660
Andrey Golovushkin <frenkodetto@yandex.ru>
2025-05-15 16:16:10 +0300

    add djoser jwt urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -10,5 +10,6 @@ router.register('recipes', views.RecipeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
