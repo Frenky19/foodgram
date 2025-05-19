88fbf5fbe4819d2fa8426e6cc741fb1032056944
Andrey Golovushkin <frenkodetto@yandex.ru>
2025-05-19 17:23:24 +0300

    docstrings and bit fix

--- backend/foodgram/meals/models.py
+++ backend/foodgram/meals/models.py
@@ -1,55 +1,69 @@
-from django.contrib.auth import get_user_model
-from django.db import models
+from decimal import Decimal
+
 from django.core.validators import MinValueValidator
+from django.db import models
 from django.utils.text import Truncator, slugify
-from meals.constants import LIMIT_OF_SYMBOLS
+
 from users.models import User
+from utils.constants import (LIMIT_OF_SYMBOLS, INGREDIENT_NAME_LIMIT,
+                             UNIT_LIMIT, TAG_NAME_LIMIT, TAG_SLUG_LIMIT,
+                             UNIT_LIMIT, RECIPE_NAME_LIMIT, DESCRIPTION_LIMIT,
+                             MIN_AMOUNT, MIN_COOK_TIME, MAX_DIGITS_FOR_AMOUNT,
+                             DECIMAL_PLACES_FOR_AMOUNT)
 
 
 class Ingredient(models.Model):
-    """Модель Ингридиента."""
+    """Модель ингредиента с возможностью выбора единиц измерения."""
 
     UNITS = [
-        ('g', 'Граммы'),
-        ('kg', 'Килограммы'),
-        ('ml', 'Миллилитры'),
-        ('l', 'Литр'),
-        ('item', 'Штуки'),
-        ('tbsp', 'Столовая ложка'),
-        ('tsp', 'Чайная ложка'),
+        ('Гр', 'Граммы'),
+        ('Кг', 'Килограммы'),
+        ('Мл', 'Миллилитры'),
+        ('Л', 'Литры'),
+        ('Шт', 'Штуки'),
+        ('СЛ', 'Столовые ложки'),
+        ('ЧЛ', 'Чайные ложки'),
     ]
 
     UNIT_FORMS = {
-        'g': ['грамм', 'грамма', 'граммов'],
-        'kg': ['килограмм', 'килограмма', 'килограммов'],
-        'ml': ['миллилитр', 'миллилитра', 'миллилитров'],
-        'l': ['литр', 'литра', 'литров'],
-        'item': ['штука', 'штуки', 'штук'],
-        'tbsp': ['столовая ложка', 'столовые ложки', 'столовых ложек'],
-        'tsp': ['чайная ложка', 'чайные ложки', 'чайных ложек'],
+        'Гр': ['грамм', 'грамма', 'граммов'],
+        'Кг': ['килограмм', 'килограмма', 'килограммов'],
+        'Мл': ['миллилитр', 'миллилитра', 'миллилитров'],
+        'Л': ['литр', 'литра', 'литров'],
+        'Шт': ['штука', 'штуки', 'штук'],
+        'СТ': ['столовая ложка', 'столовые ложки', 'столовых ложек'],
+        'ЧЛ': ['чайная ложка', 'чайные ложки', 'чайных ложек'],
     }
 
     name = models.CharField(
-        max_length=128,
+        max_length=INGREDIENT_NAME_LIMIT,
         unique=True,
         verbose_name='Название ингридиента'
     )
     unit = models.CharField(
-        max_length=64,
+        max_length=UNIT_LIMIT,
         unique=True,
         choices=UNITS,
         verbose_name='Единица измерения'
     )
 
     class Meta:
-        """."""
+        """Мета-класс для настроек модели ингредиента."""
 
         ordering = ['name']
         verbose_name = 'Ингридиент'
         verbose_name_plural = 'Ингридиенты'
 
-    def get_unit_dynamic(self, amount):
-        """Возвращает правильную форму единицы измерения для числа 'amount'."""
+    def get_unit_with_amount_display(self, amount):
+        """
+        Возвращает правильную грамматическую форму единицы измерения.
+
+        Args:
+            amount (float): Количество ингредиента
+
+        Returns:
+            str: Склоненная форма единицы измерения
+        """
         unit_forms = self.UNIT_FORMS.get(self.unit, ['', '', ''])
         n = abs(amount) % 100
         n1 = n % 10
@@ -63,23 +77,20 @@ class Ingredient(models.Model):
             return unit_forms[2]
 
     def __str__(self):
-        """Возвращает ограниченное строковое представление Ингридиента."""
-        return (
-            f'{Truncator(self.name).words(LIMIT_OF_SYMBOLS)} -'
-            f'{Truncator(self.get_unit_dynamic()).words(LIMIT_OF_SYMBOLS)}'
-        )
+        """Строковое представление ингредиента."""
+        return (f'{Truncator(self.name).words(LIMIT_OF_SYMBOLS)}')
 
 
 class Tag(models.Model):
-    """Модель Тэга."""
+    """Модель тега для категоризации рецептов."""
 
     name = models.CharField(
-        max_length=32,
+        max_length=TAG_NAME_LIMIT,
         unique=True,
         verbose_name='Название'
     )
     slug = models.SlugField(
-        max_length=32,
+        max_length=TAG_SLUG_LIMIT,
         unique=True,
         help_text=(
             'Человекочитаемое название страницы для URL; '
@@ -89,25 +100,25 @@ class Tag(models.Model):
     )
 
     class Meta:
-        """."""
+        """Мета-класс для настроек модели тега."""
 
         ordering = ['name']
         verbose_name = 'Тэг'
         verbose_name_plural = 'Тэги'
 
     def save(self, *args, **kwargs):
-        """Автоматически генерирует слаг из названия Тэга."""
+        """Автоматически генерирует slug из названия при сохранении."""
         if not self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
     def __str__(self):
-        """Возвращает ограниченное строковое представление Тэга."""
+        """Строковое представление тега."""
         return Truncator(self.name).words(LIMIT_OF_SYMBOLS)
 
 
 class Recipe(models.Model):
-    """Модель Рецепта."""
+    """Модель рецепта с привязкой к автору и тегам."""
 
     author = models.ForeignKey(
         User,
@@ -115,7 +126,7 @@ class Recipe(models.Model):
         verbose_name='Автор рецепта'
     )
     name = models.CharField(
-        max_length=256,
+        max_length=RECIPE_NAME_LIMIT,
         verbose_name='Название рецепта'
     )
     image = models.ImageField(
@@ -125,7 +136,7 @@ class Recipe(models.Model):
         verbose_name='Изображение блюда'
     )
     description = models.TextField(
-        max_length=512,
+        max_length=DESCRIPTION_LIMIT,
         verbose_name='Описание рецепта'
     )
     tags = models.ManyToManyField(
@@ -134,12 +145,12 @@ class Recipe(models.Model):
     )
     cook_time = models.PositiveSmallIntegerField(
         verbose_name='Время приготовления в минутах',
-        validators=[MinValueValidator(1)]
+        validators=[MinValueValidator(MIN_COOK_TIME)]
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        """."""
+        """Мета-класс для настроек модели рецепта."""
 
         ordering = ['name']
         verbose_name = 'Рецепт'
@@ -147,73 +158,109 @@ class Recipe(models.Model):
         default_related_name = '%(class)ss'
 
     def __str__(self):
-        """Возвращает ограниченное строковое представление Рецепта."""
+        """Строковое представление рецепта."""
         return Truncator(self.name).words(LIMIT_OF_SYMBOLS)
 
 
 class RecipeIngredient(models.Model):
-    """Промежуточная модель для связи кол-ва Ингридиента и Рецепта."""
+    """Связующая модель для ингредиентов в рецепте."""
 
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='recipes',
+        related_name='ingredients',
         verbose_name='Рецепт'
     )
     ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.CASCADE,
+        related_name='ingredients',
         verbose_name='Ингредиент'
     )
-    amount = models.PositiveSmallIntegerField(
+    amount = models.DecimalField(
         verbose_name='Количество',
-        validators=[MinValueValidator(1)]
+        validators=[MinValueValidator(Decimal(MIN_AMOUNT))],
+        max_digits=MAX_DIGITS_FOR_AMOUNT,
+        decimal_places=DECIMAL_PLACES_FOR_AMOUNT
     )
 
     class Meta:
-        """."""
+        """Мета-класс для настроек связи рецепта и ингредиента."""
 
-        verbose_name = 'Ингредиент рецепта'
-        verbose_name_plural = 'Ингредиенты рецепта'
+        verbose_name = 'Ингредиент для рецепта'
+        verbose_name_plural = 'Ингредиенты для рецепта'
 
     def __str__(self):
-        """Возвращает строковое представление количества ингридиента."""
-        return f'{self.ingredient} - {self.amount}'
+        """Строковое представление с количеством и единицей измерения."""
+        unit_disp = self.ingredient.get_unit_with_amount_display(self.amount)
+        truncated_words = Truncator(unit_disp).words(LIMIT_OF_SYMBOLS)
+        return (
+            f'{Truncator(self.ingredient).words(LIMIT_OF_SYMBOLS)} - '
+            f'{Truncator(self.amount).words(LIMIT_OF_SYMBOLS)} '
+            f'{truncated_words}'
+        )
 
 
 class Favorite(models.Model):
-    """."""
-
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
-    )
-    recipe = models.ForeignKey(
-        Recipe,
-        on_delete=models.CASCADE
-    )
-
-
-class ShoppingCart(models.Model):
-    """."""
+    """Модель для хранения избранных рецептов пользователей."""
 
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт'
+    )
+
+    class Meta:
+        """Мета-класс для настроек модели избранного."""
+
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранное'
+        default_related_name = '%(class)ss'
+
+    def __str__(self):
+        """Строковое представление избранного."""
+        return (
+            f'Рецепт {Truncator(self.recipe).words(LIMIT_OF_SYMBOLS)}'
+            f' пользователя {Truncator(self.user).words(LIMIT_OF_SYMBOLS)}'
+        )
+
+
+class ShoppingCart(models.Model):
+    """Модель корзины покупок с рецептами пользователя."""
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
         related_name='shopping_carts'
     )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='in_shopping_cart'
+        related_name='in_shopping_cart',
+        verbose_name='Рецепт'
     )
 
     class Meta:
-        """."""
+        """Мета-класс для настроек корзины покупок."""
 
+        verbose_name = 'Корзина покупок'
+        verbose_name_plural = 'Корзина покупок'
         constraints = [
             models.UniqueConstraint(
                 fields=['user', 'recipe'],
                 name='unique_shopping_cart'
             )
         ]
+
+    def __str__(self):
+        """Строковое представление корзины покупок."""
+        return (
+            f'Рецепт {Truncator(self.recipe).words(LIMIT_OF_SYMBOLS)}'
+            f' пользователя {Truncator(self.user).words(LIMIT_OF_SYMBOLS)}'
+        )
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,16 +1,16 @@
 from django.contrib.auth.models import AbstractUser
-# from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.text import Truncator
-# from api.validators import validate_username
-# from users.service import get_max_length
-# from meals.constants import (ALLOWED_SYMBOLS_FOR_USERNAME, EMAIL_LENGTH,
-#                               LIMIT_OF_SYMBOLS, USERNAME_LENGTH)
-from meals.constants import LIMIT_OF_SYMBOLS
+from utils.constants import LIMIT_OF_SYMBOLS
 
 
 class User(AbstractUser):
-    """."""
+    """Модель пользователя системы с расширенной функциональностью.
+
+    Наследует все поля стандартной модели пользователя Django и добавляет:
+    - Поле для загрузки аватара профиля
+    - Кастомные методы строкового представления
+    """
 
     profile_image = models.ImageField(
         upload_to='profile/images/',
@@ -20,18 +20,27 @@ class User(AbstractUser):
     )
 
     class Meta:
-        """."""
+        """Мета-класс для настройки модели пользователя."""
 
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
     def __str__(self):
-        """."""
+        """Строковое представление пользователя.
+
+        Возвращает:
+            str: Усеченное имя пользователя до LIMIT_OF_SYMBOLS символов
+        """
         return Truncator(self.username).words(LIMIT_OF_SYMBOLS)
 
 
 class Subscription(models.Model):
-    """."""
+    """Модель для хранения информации о подписках пользователей.
+
+    Связывает двух пользователей отношениями «подписчик» -> «автор» с проверкой
+    уникальности связи пользователей. Запрещает подписку на самого себя на
+    уровне БД.
+    """
 
     user = models.ForeignKey(
         User,
@@ -47,7 +56,7 @@ class Subscription(models.Model):
     )
 
     class Meta:
-        """."""
+        """Мета-класс для настройки модели подписок."""
 
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
@@ -57,3 +66,17 @@ class Subscription(models.Model):
                 name='unique_subscription'
             )
         ]
+
+    def __str__(self):
+        """Строковое представление подписки.
+
+        Возвращает:
+            str: Форматированная строка вида
+            "Пользователь ___ подписан на Пользователь ___"
+            с обрезанными именами до LIMIT_OF_SYMBOLS
+        """
+        return (
+            f'Пользователь {Truncator(self.user).words(LIMIT_OF_SYMBOLS)}'
+            'подписан на пользователя'
+            f'{Truncator(self.author).words(LIMIT_OF_SYMBOLS)}'
+        )
