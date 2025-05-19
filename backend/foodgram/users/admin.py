925403e1ba7005fe72a894fcb8a46b55d3b58f62
Andrey Golovushkin <frenkodetto@yandex.ru>
2025-05-19 17:23:33 +0300

    admin for users

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,65 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.html import format_html
 
-# Register your models here.
+from users.models import Subscription, User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Административная панель для управления пользователями системы."""
+
+    model = User
+    list_display = (
+        'username', 'profile_image_preview', 'first_name',
+        'last_name', 'email'
+    )
+    list_filter = ('date_joined', 'last_login',
+                   'is_staff', 'is_superuser', 'is_active')
+    readonly_fields = ('profile_image_preview', 'date_joined', 'last_login')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+    def profile_image_preview(self, obj):
+        """
+        Отображает изображения профиля пользователя в админке.
+
+        Возвращает:
+            HTML-тег изображения, если файл существует, или текст-заглушку.
+        """
+        if obj.profile_image:
+            return format_html(
+                ('<img src="{}" style="max-height:'
+                 '100px; max-width: 100px;" />'),
+                obj.profile_image.url
+            )
+        return "Нет изображения"
+    profile_image_preview.short_description = 'Фото профиля'
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    """Административная панель для управления подписками пользователей."""
+
+    list_display = ('user', 'author')
+    list_filter = ('user', 'author')
+    search_fields = (
+        'user__username',
+        'user__email',
+        'user__first_name',
+        'user__last_name',
+        'author__username',
+        'author__email',
+        'author__first_name',
+        'author__last_name'
+    )
+    autocomplete_fields = ('user', 'author')
+
+    def get_queryset(self, request):
+        """
+        Оптимизирует запросы к базе данных с использованием select_related.
+
+        Возвращает:
+            QuerySet: Оптимизированный набор данных с предварительной загрузкой
+            связанных объектов пользователя и автора.
+        """
+        return super().get_queryset(request).select_related('user', 'author')
