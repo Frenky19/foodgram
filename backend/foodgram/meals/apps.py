943e79f9c4edb637c80c024f6b410ba640a8346a
Andrey Golovushkin <frenkodetto@yandex.ru>
2025-05-19 17:23:09 +0300

    docstrings

--- backend/foodgram/meals/apps.py
+++ backend/foodgram/meals/apps.py
@@ -2,6 +2,8 @@ from django.apps import AppConfig
 
 
 class MealsConfig(AppConfig):
+    """Конфиг приложения блюд."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'meals'
     verbose_name = 'Блюда'
