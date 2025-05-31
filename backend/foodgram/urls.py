87a785ddd3ffbe6dc9f6dd198e963ba553739f39
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-05-31 19:42:44 +0300

    add avatar/me path

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -13,8 +13,13 @@ schema_view = get_schema_view(
     public=True,
 )
 
+# Надо доработать документацию, сейчас не работает
 urlpatterns = [
+    path('redoc/', schema_view.with_ui('redoc'), name='redoc'),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
-    path('redoc/', schema_view.with_ui('redoc'), name='redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
