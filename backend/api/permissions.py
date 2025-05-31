0368cadd6e88d3bc928debd8788e6e31dd8c82ec
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-05-31 19:41:23 +0300

    custom permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,31 +1,15 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    """
-    Разрешение на изменение только для автора.
-
-    Остальным только чтение.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        """."""
+class IsAdminAuthorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
+            request.method in SAFE_METHODS or
+            request.user.is_authenticated
         )
 
-
-class IsAuthenticatedOrReadOnly(permissions.BasePermission):
-    """
-    Разрешение на создание только для авторизованных пользователей.
-
-    Остальным только чтение.
-    """
-
-    def has_permission(self, request, view):
-        """."""
+    def has_object_permission(self, request, view, obj):
         return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
+            request.method in SAFE_METHODS or
+            obj.author == request.user or
+            request.user.is_staff
         )
