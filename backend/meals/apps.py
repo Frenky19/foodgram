9db723e7e03be4af1a1f9bfb4eceee23bb40652f
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-05-31 19:43:49 +0300

    delete meals app

--- backend/meals/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class MealsConfig(AppConfig):
-    """Конфиг приложения блюд."""
-
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'meals'
-    verbose_name = 'Блюда'
