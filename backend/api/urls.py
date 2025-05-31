b48841953fe9858d5f6029ae14500b6dbb026a18
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2025-05-31 19:41:53 +0300

    urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,20 +1,25 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (IngredientViewSet, RecipeViewSet, ShoppingCartViewSet,
-                       TagViewSet, UserViewSet)
-
-router = DefaultRouter()
-router.register('users', UserViewSet)
-router.register('tags', TagViewSet)
-router.register('ingredients', IngredientViewSet)
-router.register('recipes', RecipeViewSet)
-router.register(
-    'shopping_cart', ShoppingCartViewSet, basename='shopping-cart'
+from api.views import (
+    IngredientViewSet,
+    RecipeViewSet,
+    TagViewSet,
+    UserViewSet
 )
 
+app_name = 'api'
+
+router = DefaultRouter()
+
+router.register('users', UserViewSet, 'users')
+router.register('ingredients', IngredientViewSet, 'ingredients')
+router.register('recipes', RecipeViewSet, 'recipes')
+router.register('tags', TagViewSet, 'tags')
+
 urlpatterns = [
+    path('users/me/avatar/', UserViewSet.as_view({'put': 'avatar','delete': 'delete_avatar'}), name='user-avatar'),
     path('auth/', include('djoser.urls.authtoken')),
-    path('', include(router.urls)),
     path('', include('djoser.urls')),
+    path('', include(router.urls)),
 ]
