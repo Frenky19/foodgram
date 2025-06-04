5b3abc226576e2f2da67d6a4405d29c4d699fc3c
Andrey Golovushkin <frenkodetto@yandex.ru>
2025-06-04 18:08:48 +0300

    delete autoschema

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,20 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title='Foodgram',
-        default_version='',
-    ),
-    public=True,
-)
 
-# Надо доработать документацию, сейчас не работает
 urlpatterns = [
-    path('redoc/', schema_view.with_ui('redoc'), name='redoc'),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
