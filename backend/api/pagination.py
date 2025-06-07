ffb9644f37da899207bb5223e8d5c5f3e042050e
Andrey Golovushkin <frenkodetto@yandex.ru>
2025-06-07 14:59:26 +0300

    page size to paginations

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -4,5 +4,6 @@ from rest_framework.pagination import PageNumberPagination
 class CustomPagination(PageNumberPagination):
     """Пагинация с параметрами limit и page."""
 
+    page_size = 6
     page_size_query_param = 'limit'
     page_query_param = 'page'
